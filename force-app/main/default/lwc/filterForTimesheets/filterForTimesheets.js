43a82fb473367fe54bf4d0eb9ec0a7a4203ca144
Alejandro Sanchez <2198022-1043PM@users.noreply.gitlab.com>
2022-06-14 15:45:51 -0500

    add combobox

--- force-app/main/default/lwc/filterForTimesheets/filterForTimesheets.js
+++ force-app/main/default/lwc/filterForTimesheets/filterForTimesheets.js
@@ -1,3 +1,13 @@
 import { LightningElement } from 'lwc';
 
-export default class FilterForTimesheets extends LightningElement {}
+export default class FilterForTimesheets extends LightningElement {
+    value = 'inProgress';
+
+    get options() {
+        return [
+            { label: 'New', value: 'new' },
+            { label: 'In Progress', value: 'inProgress' },
+            { label: 'Finished', value: 'finished' },
+        ];
+    }
+}
