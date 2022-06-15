e4bb25b2621f0443ac5fff33e7aef3f4e20ef272
Karla Bello <karlabello1024006@gmail.com>
2022-06-15 13:27:05 -0400

    filterfortimesheet update 1

--- force-app/main/default/lwc/filterForTimesheets/filterForTimesheets.js
+++ force-app/main/default/lwc/filterForTimesheets/filterForTimesheets.js
@@ -1,7 +1,8 @@
 import { LightningElement } from 'lwc';
 
 export default class FilterForTimesheets extends LightningElement {
-    value = 'inProgress';
+    value = 'last Modified';
+    value2 = 'approved';
 
     get options() {
         return [
@@ -10,4 +11,15 @@ export default class FilterForTimesheets extends LightningElement {
             { label: 'Last Modified', value: 'last Modified' },
         ];
     }
+    
+
+    get optionsstatus() {
+        return [
+            { label: 'Pending', value: 'pending' },
+            { label: 'Approved', value: 'approved' },
+            { label: 'Rejected', value: 'rejected' },
+            { label: 'Draft', value: 'draft' },
+        ];
+    }
 }
+//Pending, Approved RejectedDraft  \\
