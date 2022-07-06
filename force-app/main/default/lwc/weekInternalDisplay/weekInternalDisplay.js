a7992410b3740f916b0f9aaeaa77d99614d5b6f8
Karla Bello <karlabello1024006@gmail.com>
2022-07-06 15:15:45 -0400

    add weekInternalDisplay Component

--- force-app/main/default/lwc/weekInternalDisplay/weekInternalDisplay.js
+++ force-app/main/default/lwc/weekInternalDisplay/weekInternalDisplay.js
@@ -1,3 +1,28 @@
 import { LightningElement } from 'lwc';
 
-export default class WeekInternalDisplay extends LightningElement {}
+export default class WeekInternalDisplay extends LightningElement {
+    curr = new Date()
+
+    get intervalDate() {
+        const first = this.curr.getDate() - this.curr.getDay()
+        const last = first + 6
+
+        const firstday = new Date(this.curr.setDate(first))
+        const lastday = new Date(this.curr.setDate(last))
+        return `${firstday.getDate()}/${firstday.getMonth() + 1}/${firstday.getFullYear()} - ${lastday.getDate()}/${lastday.getMonth() + 1}/${lastday.getFullYear()}`
+    }
+
+    substractWeek() {
+        const lastWeek = this.curr.getDate() - this.curr.getDay()
+        const last = lastWeek - 6
+
+        this.curr = new Date(this.curr.setDate(last))
+    }
+
+    addWeek() {
+        const nextWeek = this.curr.getDate() - this.curr.getDay()
+        const next = nextWeek + 12
+
+        this.curr = new Date(this.curr.setDate(next))
+    }
+}
